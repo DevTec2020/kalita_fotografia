19946b42a2bba089a863b8971bf7f1ebd758a216
CaioMiranda12 <caioclm04@gmail.com>
2025-07-03 11:30:21 -0300

    fix: fixed responsive on sobre page

--- src/pages/Sobre/Sobre.tsx
+++ src/pages/Sobre/Sobre.tsx
@@ -3,39 +3,78 @@ import HomeImg from '../../assets/Picture-about-me.svg'
 
 const Sobre = () => {
   return (
+    <>
 
-    <main>
-      <div className='flex justify-center'>
-        <div className='bg-[#E5DACE] w-full h-[calc(100vh-180px)] max-w-7xl mx-8'>
-          <div className='h-full flex items-center px-3 xl:mx-8'>
+      {/* Mobile */}
 
-            <div className='overflow-visible'>
-              <img 
-                src={HomeImg}
-                alt='imagem da kalita' 
-                className='-translate-x-6 lg:-translate-x-20 xl:-translate-x-35'
-              />
-            </div>
+      <main className='py-10 md:py-0 bg-quaternary md:bg-white'>
+        {/* md:bg-white */}
 
-            <div>
-              <h1 className='text-primary text-4xl sm:text-5xl xl:text-7xl font-homeKalita'>Kalita</h1>
-              <p className='font-bold text-base xl:text-2xl mt-6 mb-[42px]'>Sorem ipsum dolor sit amet, consectetur.</p>
+
+        <div className='md:hidden'>
+          <h1 className='text-primary text-[64px] font-bold font-lata leading-[110%] text-center mb-4 md:hidden'>Sobre mim</h1>
+          <div className='w-full'>
+            <img
+              src={HomeImg}
+              alt='imagem da kalita'
+              className='w-full h-auto'
+            />
+
+            <div className='p-5'>
+              <p
+                className='text-base text-left text-[#242424] font-lata leading-relaxed'
+              >
+                Qorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur tempus urna at turpis condimentum lobortis. Ut commodo efficitur neque. Ut diam quam, semper iaculis condimentum ac, vestibulum eu nisl.
+              </p>
 
               <Link
                 to='/contato'
-                className='bg-secondary py-2 px-3 sm:py-3 sm:px-6 rounded-[48px] text-tertiary font-bold text-base cursor-pointer hover:opacity-80 active:opacity-60 w-max block text-center'>
-                  Vamos conversar
+                className='bg-secondary py-3 px-6 rounded-[48px] text-tertiary font-bold font-lata text-base cursor-pointer hover:opacity-80 active:opacity-60 w-full block text-center mt-5'>
+                Vamos conversar
               </Link>
             </div>
-          
           </div>
-
-          
         </div>
-      </div>
 
-    </main>
-    
+
+        {/* Desktop */}
+
+        <div className='hidden md:flex justify-center px-6 lg:px-12 xl:px-20'>
+          <div className='bg-[#E5DACE] w-full h-[calc(100vh-180px)] max-w-7xl mx-auto  '>
+            <div className='h-full flex items-center px-3'>
+
+              <div className='overflow-visible relative flex-shrink-0'>
+                <img
+                  src={HomeImg}
+                  alt='imagem da kalita'
+                  className='-translate-x-6 lg:-translate-x-20 xl:-translate-x-36'
+                />
+              </div>
+
+
+              <div>
+                <h1 className='text-primary text-[64px] font-homeKalita mb-6'>Kalita</h1>
+                <p className='font-bold font-lata text-[#242424] text-2xl mb-[42px]'>Sorem ipsum dolor sit amet, consectetur.</p>
+
+                <Link
+                  to='/contato'
+                  className='bg-secondary py-3 px-6 rounded-[48px] text-tertiary font-bold text-base cursor-pointer hover:opacity-80 active:opacity-60 w-max block text-center mx-auto'>
+                  Vamos conversar
+                </Link>
+              </div>
+
+            </div>
+
+
+          </div>
+        </div>
+
+      </main>
+
+
+
+
+    </>
   )
 }
 export default Sobre
