6475aa59e6e7a6a50713981ddc85f3a102199749
Bruno Gomes <bgomes.89@gmail.com>
2025-07-11 10:19:50 -0300

    fix: ajuste semantica componente nav

--- src/components/Nav/nav.tsx
+++ src/components/Nav/nav.tsx
@@ -2,57 +2,57 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import line from "../../assets/lineDetails.svg";
 
+
 export function Nav(){
   const [isOpen, setIsOpen] = useState(false);
+
+  const menuItems = [
+    {href: "/portfolio", label: "Portfólio"},
+    {href: "/sobre", label: "Sobre"},
+    {href: "/depoimentos", label: "Depoimentos"},
+    {href: "/contato", label: "Contato"},
+  ]
 
   function toggleMenu() {
     setIsOpen((prev) => !prev);
   }
-
     return (
       <nav className="absolute right-6 ">
-          <ul className="gap-5 uppercase hidden sm:flex">
-            <Link to="/portfolio">
-              <li className="text-md text-primary font-bold hover:cursor-pointer hover:text-secondary transform transition duration-300">Portfólio</li>
-            </Link>
-            <Link to="/sobre">
-              <li className="text-md text-primary font-bold hover:cursor-pointer hover:text-secondary transform transition duration-300">Sobre</li>
-            </Link>
-            <Link to="/depoimentos">
-              <li className="text-md text-primary font-bold hover:cursor-pointer hover:text-secondary transform transition duration-300">Depoimentos</li>
-            </Link>
-            <Link to="/contato">
-              <li className="text-md text-primary font-bold hover:cursor-pointer hover:text-secondary transform transition duration-300">Contato</li>
-            </Link>
-          </ul>
-
           <button onClick={toggleMenu} className="sm:hidden block">
-            {isOpen ? <p className="font-homeKalita text-2xl">X</p> : <img src={line} alt="icone para menu" />}
+            {isOpen ? (
+              <p className="font-homeKalita text-2xl">X</p>
+            ) : (
+              <img src={line} alt="icone para menu" />
+            )}
           </button>
-          {/* Overlay */}
             {isOpen && (
                 <div
-                    className="fixed top-28 inset-0 bg-black/40 z-40"
+                    className="fixed top-28 inset-0 bg-black/40 z-40 sm:hidden"
                     onClick={toggleMenu}
                 >
                   <section className= "absolute right-0 bg-quaternary h-svh w-1/2 animate-openMenu">
                    <ul className="uppercase flex flex-col items-center justify-center gap-8 pt-8">
-                      <Link to="/portfolio">
-                        <li className="text-md text-primary font-bold hover:cursor-pointer hover:text-secondary transform transition duration-300">Portfólio</li>
-                      </Link>
-                      <Link to="/sobre">
-                        <li className="text-md text-primary font-bold hover:cursor-pointer hover:text-secondary transform transition duration-300">Sobre</li>
-                      </Link>
-                      <Link to="/depoimentos">
-                        <li className="text-md text-primary font-bold hover:cursor-pointer hover:text-secondary transform transition duration-300">Depoimentos</li>
-                      </Link>
-                      <Link to="/contato">
-                        <li className="text-md text-primary font-bold hover:cursor-pointer hover:text-secondary transform transition duration-300">Contato</li>
-                      </Link>
-                  </ul>
+                    {menuItems.map(({href, label}) => (
+                       <li key={href} className="text-md text-primary font-bold hover:cursor-pointer hover:text-secondary transform transition duration-300">
+                        <Link to={href} >
+                          {label}
+                        </Link>
+                       </li>
+                    ))}
+                   </ul>
                   </section>
                 </div>
             )}
+
+            <ul className="gap-5 uppercase hidden sm:flex">
+             {menuItems.map(({href, label}) => (
+                <li key={href} className="text-md text-primary font-bold hover:cursor-pointer hover:text-secondary transform transition duration-300">
+                  <Link to={href}>
+                    {label}
+                  </Link>
+                </li>
+            ))}
+           </ul>
         </nav>
     )
 }
