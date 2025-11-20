0e88ccdd9b0293bbe262a3d8664f0e08aa71c092
CaioMiranda12 <caioclm04@gmail.com>
2025-11-20 13:31:11 -0300

    feat: create sidebar component to dashboard

---
+++ src/domain/dashboard/components/Sidebar.tsx
@@ -0,0 +1,67 @@
+
+import Image from "next/image";
+import Link from "next/link";
+
+import { RiGalleryView2 } from "react-icons/ri";
+import { GrGallery } from "react-icons/gr";
+import { PiHouseLight } from "react-icons/pi";
+import { PiPackage } from "react-icons/pi";
+import { LuMessageSquareText } from "react-icons/lu";
+import { MdLogout } from "react-icons/md";
+
+
+const navLinks = [
+  { href: '/dashboard/painel', label: 'Painel', icon: <RiGalleryView2 /> },
+  { href: '/dashboard/gestao-portfolio', label: 'Gestão do Portfólio', icon: <GrGallery /> },
+  { href: '/dashboard/fotos', label: 'Fotos da Home', icon: <PiHouseLight /> },
+  { href: '/dashboard/gestao-pacotes', label: 'Gestão de Pacotes Sazonais', icon: <PiPackage /> },
+  { href: '/dashboard/controle-feedbacks', label: 'Controle de feedbacks', icon: <LuMessageSquareText /> },
+];
+
+export function Sidebar({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex min-h-screen w-full">
+      <aside
+        className='flex flex-col justify-between bg-white w-[25%] min-w-[260px] py-10'
+      >
+        <div className="px-8">
+          <header className="flex items-center gap-1">
+            <Image
+              src='/kalita-logo.svg'
+              alt='Logo da kalita'
+              width={90}
+              height={90}
+              priority
+            />
+
+            <h1 className="text-kalita-brown-dark text-lg font-semibold">Área Administrativa</h1>
+          </header>
+
+          <nav className="mt-14">
+            <h2 className="mb-6 text-kalita-brown-dark font-semibold text-lg">Gerenciamento</h2>
+
+            <div className="flex flex-col gap-2 items-start">
+              {navLinks.map(({ href, label, icon }) => (
+                <Link key={label} href={href} className="inline-flex items-center gap-3 text-base font-normal">
+                  {icon}
+                  {label}
+                </Link>
+              ))}
+            </div>
+          </nav>
+        </div>
+
+        <div className="border-t border-kalita-bg-light-brown w-full pt-6 px-8">
+          <button className="flex items-center gap-2 text-[15px] cursor-pointer text-base text-kalita-brown-dark font-bold">
+            <MdLogout />
+            Sair
+          </button>
+        </div>
+      </aside>
+
+      <main className="bg-red-300 flex-1">
+        {children}
+      </main>
+    </div>
+  )
+}
