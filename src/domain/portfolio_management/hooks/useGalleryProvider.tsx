151906b89066c45281b3f4ff4fbd28ace69a4d81
Daniel <danielpapereiraJV@gmail.com>
2025-11-28 19:29:50 -0300

    upload/image - Gallery Provider - provider of methods, interfaces, and objects that share the same nature as the portfolio management component. (Hook)

---
+++ src/domain/portfolio_management/hooks/useGalleryProvider.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import { createContext, useContext, useState, ReactNode } from "react";
+import { v4 as uuid } from "uuid";
+import { GalleryContextData, GalleryImage } from "../types/gallery-types";
+
+const GalleryContext = createContext<GalleryContextData | null>(null);
+
+export function GalleryProvider({ children }: { children: ReactNode }) {
+
+  const [images, setImages] = useState<GalleryImage[]>([]);
+
+  const selectedIds = images.filter(img => img.selected).map(img => img.id);
+
+  function addImage(file: File) {
+    const newImg: GalleryImage = {
+      id: uuid(),
+      file,
+      preview: URL.createObjectURL(file),
+      selected: false,
+    };
+    setImages(prev => [...prev, newImg]);
+  }
+
+
+  //TODO: DUVIDA
+  function toggleSelect(id: string) {
+    setImages(prev =>
+      prev.map(img =>
+        img.id === id ? { ...img, selected: !img.selected } : img
+      )
+    );
+  }
+
+  function toggleSelectAll() {
+    const allSelected = images.every(img => img.selected);
+
+    setImages(prev =>
+      prev.map(img => ({
+        ...img,
+        selected: !allSelected,
+      }))
+    );
+  }
+
+  function removeImages(ids: string[]) {
+    setImages(prev => prev.filter(img => !ids.includes(img.id)));
+  }
+
+  return (
+    <GalleryContext.Provider
+      value={{
+        images,
+        selectedIds,
+        addImage,
+        toggleSelect,
+        toggleSelectAll,
+        removeImages,
+      }}
+    >
+      {children}
+    </GalleryContext.Provider>
+  );
+}
+
+
+export function useGallery() {
+  const ctx = useContext(GalleryContext);
+  if (!ctx) throw new Error("useGallery deve estar dentro de <GalleryProvider>");
+  return ctx;
+}
