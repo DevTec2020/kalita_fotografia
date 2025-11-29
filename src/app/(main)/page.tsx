2c41b23675cb4cfc7296f10592435a006ff7a5dc
dinhoSiwa <claudiosilva.one@gmail.com>
2025-11-28 21:52:49 -0300

    fix: restore-home-page-and-layout

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -2,16 +2,17 @@
 import { HeroBanner } from "./_components/Hero/HeroBanner";
 import { HeaderHeadings } from "../shared/ui/home/HeaderHeadings";
 import { Testimony } from "./_components/testimony/Testimony";
-import PortfolioManagement from "@/domain/portfolio_management/PortfolioManagement";
-
 
 //ARQUIVO DE ROTAS
 export default function Home() {
   return (
-   <main className="">
-
-    <PortfolioManagement/>
+   <main className="flex flex-col gap-24">
     
+    <HeroBanner />
+    <HeaderHeadings />
+    <Testimony />
+    
+
    </main>
   );
 }
